287822f3235e444171e20848f7e405075dd47a42
Virginie17 <niniezouk@hotmail.fr>
2024-12-16 15:46:37 +0100

    enelevement admin

---
+++ middleware.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  matcher: [
+    '/admin/:path*',
+    '/api/devis/:path*'
+  ]
+}
--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['media2.dev.to', 'dev-to-uploads.s3.amazonaws.com'],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256],
-    formats: ['image/avif', 'image/webp'],
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
-  },
   output: 'standalone',
   typescript: {
     ignoreBuildErrors: true
